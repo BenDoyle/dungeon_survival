cc1dbbfa482c470f939833cb7d4e5b08112e228c
Ben Doyle <doyle.ben@gmail.com>
2014-03-16 18:37:24 -0400

    whitespace

--- extract.rb
+++ extract.rb
@@ -1,29 +1,29 @@
 def parse_morgue_file2(morgue_file)
-	singular_monster_regex   = /^\s{2}(An?\s+)?([a-zA-Z_\s]+)\(\w+:\d{1,2}\)/
-	plural_monster_regex     = /^\s{2}(\d+)\s([a-zA-Z_\s]+)(\(\w+:\d{1,2}\))?/
-	event_regex              = /^(\d+)\s+\|\s+(\w+):(\d{1,2})\s+\|\s+(.+)/
+  singular_monster_regex   = /^\s{2}(An?\s+)?([a-zA-Z_\s]+)\(\w+:\d{1,2}\)/
+  plural_monster_regex     = /^\s{2}(\d+)\s([a-zA-Z_\s]+)(\(\w+:\d{1,2}\))?/
+  event_regex              = /^(\d+)\s+\|\s+(\w+):(\d{1,2})\s+\|\s+(.+)/
 
-	monsters = []
-	events   = []
+  monsters = []
+  events   = []
 
-	File.open(morgue_file, "r") do |file|
-		while(line = file.gets)
-			if line.ascii_only?
-				case line
-				when singular_monster_regex
-					monster = line.scan(singular_monster_regex).first
-					monsters << {number: 1, monster: monster[1].strip} unless monster[1].strip.empty?
-				when plural_monster_regex
-					monster = line.scan(plural_monster_regex).first
-					monsters << {number: monster[0].strip.to_i, monster: monster[1].strip} unless monster[1].strip.empty?
-				when event_regex
-					turn, branch, level, message = line.scan(event_regex).first
-					events << {turn: turn, branch: branch, level: level, message: message}
-				end
-			end
-		end
-	end
-	[events, monsters]
+  File.open(morgue_file, "r") do |file|
+    while(line = file.gets)
+      if line.ascii_only?
+        case line
+        when singular_monster_regex
+          monster = line.scan(singular_monster_regex).first
+          monsters << {number: 1, monster: monster[1].strip} unless monster[1].strip.empty?
+        when plural_monster_regex
+          monster = line.scan(plural_monster_regex).first
+          monsters << {number: monster[0].strip.to_i, monster: monster[1].strip} unless monster[1].strip.empty?
+        when event_regex
+          turn, branch, level, message = line.scan(event_regex).first
+          events << {turn: turn, branch: branch, level: level, message: message}
+        end
+      end
+    end
+  end
+  [events, monsters]
 end
 
 
@@ -31,13 +31,13 @@ morgue_files = Dir["#{ARGV[0]}/*"].entries
 games = []
 
 morgue_files.each do |morgue_file|
-	name, timestamp, type = morgue_file.scan(/morgue-(\w+)-(\d{8}-\d{6})\.(\w{3})/).first
-	if name && timestamp && type == 'txt'
+  name, timestamp, type = morgue_file.scan(/morgue-(\w+)-(\d{8}-\d{6})\.(\w{3})/).first
+  if name && timestamp && type == 'txt'
 # morgue_file = '/Users/ben/Library/Application Support/Dungeon Crawl Stone Soup/morgue/morgue-Ben-20110508-192552.txt'
-		notes, monsters = parse_morgue_file2(morgue_file)
-		games << {name: name, timestamp: timestamp, notes: notes, monsters: monsters} if notes || monsters
+    notes, monsters = parse_morgue_file2(morgue_file)
+    games << {name: name, timestamp: timestamp, notes: notes, monsters: monsters} if notes || monsters
 # games << {notes: notes, monsters: monsters} if notes || monsters
-	end
+  end
 end
 
 puts games[2][:name]
