d586540c55f271dd52ccaf2a574df527e4942d42
Ben Doyle <doyle.ben@gmail.com>
2014-03-16 18:36:28 -0400

    remove obsolete code

--- extract.rb
+++ extract.rb
@@ -1,57 +1,3 @@
-# def parse_morgue_file(morgue_file)
-# 	file = File.new(morgue_file, "r")
-# 	notes = nil
-# 	monsters = nil
-# 	while(line = file.gets)
-# 		if line.ascii_only? && line.strip == 'Notes'
-# 			notes = parse_notes(file)
-# 		end
-# 		if line.ascii_only? && line.strip == 'Vanquished Creatures'
-# 			monsters = parse_monsters(file)
-# 		end
-# 	end
-# 	file.close
-# 	return [notes, monsters]
-# end
-
-# def parse_monsters(file)
-# 	monster   = /(A|An|\d+)\s(\w+)/
-# 	finished  = /Grand Total/
-	
-# 	rows = []
-# 	while(line = file.gets)
-# 		line = line.strip
-# 		case line
-# 		when monster
-# 			number, monster = line.scan(monster).first
-# 			rows << {number: number, monster: monster}
-# 		when finished
-# 			break
-# 		end
-# 	end
-# 	rows
-# end
-
-# def parse_notes(file)
-# 	header    = /Turn\s+|\s+Place\s+|\s+Note/
-# 	row_break = /-+/
-# 	data      = /(\d+)\s+\|\s+(\w+):(\d{1,2})\s+\|\s+(.+)/
-# 	rows = []
-# 	while(line = file.gets)
-# 		line = line.strip
-# 		case line
-# 		when data
-# 			turn, branch, level, message = line.scan(data).first
-# 			rows << {turn: turn, branch: branch, level: level, message: message}
-# 		when header
-# 		when row_break
-# 		else
-# 			break
-# 		end
-# 	end
-# 	rows
-# end
-
 def parse_morgue_file2(morgue_file)
 	singular_monster_regex   = /^\s{2}(An?\s+)?([a-zA-Z_\s]+)\(\w+:\d{1,2}\)/
 	plural_monster_regex     = /^\s{2}(\d+)\s([a-zA-Z_\s]+)(\(\w+:\d{1,2}\))?/
@@ -98,4 +44,3 @@ puts games[2][:name]
 puts games[2][:timestamp]
 # puts games[1][:notes]
 puts games[2][:monsters]
-
