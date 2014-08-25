1e872c188a4e656168a8718a0a4c8bd4bc5980ca
Benjamin Doyle <ben@Hawkeye.local>
2014-08-25 15:34:31 -0400

    break out quoted text

--- test/monster_parser_test.rb
+++ test/monster_parser_test.rb
@@ -6,16 +6,15 @@ class WorkflowTest < Test::Unit::TestCase
 		@parser ||= MonstersParser.new
 	end
 
-	def test_parse_basic
-		parser = MonstersParser.new
+	def test_row1
 		row1 = "MONS_DRACONIAN, 'd', BROWN, \"draconian\","
 		
 		elements = parser.parse(row1).elements
 		
 		assert_equal 'MONS_DRACONIAN', elements[0].text_value
-		assert_equal '\'d\'', elements[2].text_value
+		assert_equal 'd', elements[2].elements[1].text_value
 		assert_equal 'BROWN', elements[4].text_value
-		assert_equal '"draconian"', elements[6].text_value
+		assert_equal 'draconian', elements[6].elements[1].text_value
 	end
 
 end
