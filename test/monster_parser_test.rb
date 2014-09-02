861283706c8ad77aaefe076dda7cde8612eca2f1
Ben Doyle <ben.doyle@shopify.com>
2014-09-02 16:53:15 -0400

    row2 rules and test

--- test/monster_parser_test.rb
+++ test/monster_parser_test.rb
@@ -6,15 +6,25 @@ class WorkflowTest < Test::Unit::TestCase
 		@parser ||= MonstersParser.new
 	end
 
-	def test_row1
-		row1 = "MONS_DRACONIAN, 'd', BROWN, \"draconian\","
-		
-		elements = parser.parse(row1).elements
-		
-		assert_equal 'MONS_DRACONIAN', elements[0].text_value
-		assert_equal 'd', elements[2].elements[1].text_value
-		assert_equal 'BROWN', elements[4].text_value
-		assert_equal 'draconian', elements[6].elements[1].text_value
-	end
+	# def test_row1
+	# 	row1 = "MONS_DRACONIAN, 'd', BROWN, \"draconian\","
 
+	# 	elements = parser.parse(row1).elements
+
+	# 	assert_equal 'MONS_DRACONIAN', elements[0].text_value
+	# 	assert_equal 'd', elements[2].elements[1].text_value
+	# 	assert_equal 'BROWN', elements[4].text_value
+	# 	assert_equal 'draconian', elements[6].elements[1].text_value
+	# end
+
+	def test_row2
+		row2 = "M_SENSE_INVIS | M_WARM_BLOOD | M_BATTY | M_NO_POLY_TO,"
+
+		elements = parser.parse(row2).elements
+
+		assert_equal 'M_SENSE_INVIS', elements[0].text_value
+		assert_equal 'M_WARM_BLOOD', elements[1].elements[0].elements[3].text_value
+		assert_equal 'M_BATTY', elements[1].elements[1].elements[3].text_value
+		assert_equal 'M_NO_POLY_TO', elements[1].elements[2].elements[3].text_value
+	end
 end
