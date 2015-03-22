38093a00bced9544b93f052530d86f195709e632
Ben Doyle <ben.doyle@shopify.com>
2015-03-22 16:40:19 -0400

    move old approach out, keep it around for reference

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,5 @@ require 'time'
 require 'test/unit'
 require 'treetop'
 
-require File.join(File.dirname(__FILE__),'..','lib','workflow.rb')
-require File.join(File.dirname(__FILE__),'..','lib','message_parser.rb')
 
 Treetop.load File.join(File.dirname(__FILE__),'..','lib','monsters.treetop')
