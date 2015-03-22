7612171631377c31e8ddb7209649b396e0ee79aa
Ben Doyle <ben.doyle@shopify.com>
2015-03-22 16:40:44 -0400

    start splitting out monster parser

--- test/monster_parser_test.rb
+++ test/monster_parser_test.rb
@@ -1,26 +1,22 @@
 require 'test_helper'
 
-class WorkflowTest < Test::Unit::TestCase
+class MonsterParserTest < Test::Unit::TestCase
 
-	def parser
-		@parser ||= MonstersParser.new
+	def test_row1
+		row1 = "MONS_DRACONIAN, 'd', BROWN, \"draconian\","
+
+		elements = MonstersRow1Parser.new.parse(row1).elements
+
+		assert_equal 'MONS_DRACONIAN', elements[0].text_value
+		assert_equal 'd', elements[2].elements[1].text_value
+		assert_equal 'BROWN', elements[4].text_value
+		assert_equal 'draconian', elements[6].elements[1].text_value
 	end
-
-	# def test_row1
-	# 	row1 = "MONS_DRACONIAN, 'd', BROWN, \"draconian\","
-
-	# 	elements = parser.parse(row1).elements
-
-	# 	assert_equal 'MONS_DRACONIAN', elements[0].text_value
-	# 	assert_equal 'd', elements[2].elements[1].text_value
-	# 	assert_equal 'BROWN', elements[4].text_value
-	# 	assert_equal 'draconian', elements[6].elements[1].text_value
-	# end
 
 	def test_row2
 		row2 = "M_SENSE_INVIS | M_WARM_BLOOD | M_BATTY | M_NO_POLY_TO,"
 
-		elements = parser.parse(row2).elements
+		elements = MonstersRow2Parser.new.parse(row2).elements
 
 		assert_equal 'M_SENSE_INVIS', elements[0].text_value
 		assert_equal 'M_WARM_BLOOD', elements[1].elements[0].elements[3].text_value
@@ -31,7 +27,7 @@ class WorkflowTest < Test::Unit::TestCase
 	def test_row2_single
 		row2 = "M_SENSE_INVIS,"
 
-		elements = parser.parse(row2).elements
+		elements = MonstersRow2Parser.new.parse(row2).elements
 
 		assert_equal 'M_SENSE_INVIS', elements[0].text_value
 	end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,6 @@ require 'time'
 require 'test/unit'
 require 'treetop'
 
-
-Treetop.load File.join(File.dirname(__FILE__),'..','lib','monsters.treetop')
+Treetop.load File.join(File.dirname(__FILE__), '..', 'lib', 'parsers', 'monsters.treetop')
+Treetop.load File.join(File.dirname(__FILE__), '..', 'lib', 'parsers', 'monsters_row1.treetop')
+Treetop.load File.join(File.dirname(__FILE__), '..', 'lib', 'parsers', 'monsters_row2.treetop')
